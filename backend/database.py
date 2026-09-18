4a3c097db3aea54aa297fe2fe847f8492ece59c3
vandana k <vandhanak2005@example.com>
2026-09-18 15:25:57 +0530

    Prepare database configuration for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,29 @@
+import os
 from urllib.parse import quote_plus
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DB_PASSWORD = "darukaa@123"
+DB_PASSWORD = os.getenv("DB_PASSWORD", "YOUR_PASSWORD")
 
-DATABASE_URL = f"postgresql://postgres:{quote_plus(DB_PASSWORD)}@localhost:5432/darukaa"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql://postgres:{quote_plus(DB_PASSWORD)}@localhost:5432/darukaa",
+)
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 Base = declarative_base()
 
 
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
